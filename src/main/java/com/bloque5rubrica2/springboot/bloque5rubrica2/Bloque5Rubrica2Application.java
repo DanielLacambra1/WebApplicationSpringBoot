4b922620aa0bd74b50acc3b11de8afefb3f4b8bd
DanielLacambra1 <lacambra-sidan21@zaragoza.salesianos.edu>
2023-01-19 09:11:35 +0100

    initial commit

---
+++ src/main/java/com/bloque5rubrica2/springboot/bloque5rubrica2/Bloque5Rubrica2Application.java
@@ -0,0 +1,13 @@
+package com.bloque5rubrica2.springboot.bloque5rubrica2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bloque5Rubrica2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bloque5Rubrica2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bloque5rubrica2/springboot/bloque5rubrica2/Bloque5Rubrica2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloque5rubrica2.springboot.bloque5rubrica2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bloque5Rubrica2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
